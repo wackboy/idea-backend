07662b7ccad99ee3f9ecf84105e9305600e6a9cd
Suiyuan <15955176942@163.com>
2020-11-25 19:34:10 +0800

    添加了评论

---
+++ platform-item/platform-service/src/main/java/wackyboy/top/item/mapper/CommentMapper.java
@@ -0,0 +1,11 @@
+package wackyboy.top.item.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import wackyboy.top.item.pojo.Comment;
+
+/**
+ * @Author: WackyBoy
+ * @Date: 2020/11/25 19:31
+ */
+public interface CommentMapper extends Mapper<Comment> {
+}
